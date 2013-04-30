1bb1ceeabb50d55554f1a7e79dbb39cd1b5cf466
Samuel Jackson <samueljackson@outlook.com>
2013-04-30 17:53:14 +0100

    Updated graphs.

--- code/output_files/better_stats.py
+++ code/output_files/better_stats.py
@@ -9,41 +9,40 @@ def autolabel(ax, rects):
         ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
                 ha='center', va='bottom')
 
+def makeBars(ax, data, offset, width, color, increment):
+	return ax.bar(np.arange(len(data))*offset + increment, data, bottom=0, width=width, align='edge', color=color)
 
-def makeplot(allData, test1, test2, test3, blocked, no_blocked, no_prior, title, y_label):
-	names = ['SJN', 'Round Robin',  'FCFS', 'Lottery Scheduling']
-	num_tests = (len(allData)/len(names))
+def makeplot(allData, title, y_label):
+	colours = [(.5, .75, 0.5, 1), (.75, .75, 0.5, 1), (.5, .75, 0.75, 1), (.75, .5, 0.75, 1), (.5, .5, 0.5, 1), (.5, .25, 0.5, 1)]
+	names = ['STR', 'Round Robin',  'FCFS', 'Lottery Scheduling']
+	test_names = ['Test1', 'Test2', 'Test3', 'Blocked', 'Non Blocked', 'No Priority']
+
+	num_algorithms = len(names)
+	num_tests= len(allData)/num_algorithms
 
 	fig = plt.figure(figsize=(13,5))
 	ax = fig.add_subplot(111)
 	width = 0.8
 
 	ax.set_xlim((-width/2, len(allData)))
-	#ax.set_ylim(ymin=0, ymax=max(allData)+20)
-	ax.set_ylim(ymin=0, ymax=50)
+	ax.set_ylim(ymin=0, ymax=max(allData)+20)
+	#ax.set_ylim(ymin=0, ymax=50)
 
-	rects1 = ax.bar(np.arange(len(test1)) * num_tests, test1, bottom=0, width=width, align='edge', color=(.5, .75, 0.5, 1))
-	rects2 = ax.bar(np.arange(len(test2)) * num_tests+width, test2, bottom=0, width=width, align='edge', color=(.75, .75, 0.5, 1))
-	rects3 = ax.bar(np.arange(len(test3)) * num_tests+width*2, test3, bottom=0, width=width, align='edge', color=(.5, .75, 0.75, 1))
-	rects4 = ax.bar(np.arange(len(blocked)) * num_tests+width*3, blocked, bottom=0, width=width, align='edge', color=(.75, .5, 0.75, 1))
-	rects5 = ax.bar(np.arange(len(no_blocked)) * num_tests+width*4, no_blocked, bottom=0, width=width, align='edge', color=(.5, .5, 0.5, 1))
-	rects6 = ax.bar(np.arange(len(no_prior)) * num_tests+width*5, no_prior, bottom=0, width=width, align='edge', color=(.5, .25, 0.5, 1))
+	allRects = []
 
-	ax.set_xticks([(i*num_tests)+width*(num_tests/2) for i in range(len(names))])
+	for i in range(num_algorithms):
+		row_data = allData[i::num_algorithms]
+		rects = makeBars(ax, row_data, num_algorithms, width, colours[i], width*i)
+		allRects.append(rects)
+		autolabel(ax, rects)
 
-	xtickNames = ax.set_xticklabels(names)
+	ticks = [i*(5*width) + (width*2) for i in range(num_tests)]
+	ax.set_xticks(ticks)
+
+	xtickNames = ax.set_xticklabels(test_names)
 	plt.setp(xtickNames, rotation=0, fontsize=10)
 
-	autolabel(ax, rects1)
-	autolabel(ax, rects2)
-	autolabel(ax, rects3)
-	autolabel(ax, rects4)
-	autolabel(ax, rects5)
-	autolabel(ax, rects6)
-
-	ax.legend( (rects1[0], rects2[0], rects3[0], rects4[0], rects5[0], rects6[0]), 
-		('Test1', 'Test2', 'Test3', 'Blocked', 'Not Blocked', 'Same Priority'), 
-		bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+	ax.legend(allRects, names, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 	ax.set_xlabel('Scheduling Algorithm')
 	ax.set_ylabel(y_label)
@@ -101,11 +100,11 @@ no_prior_fst = [11,48,13,44.201]
 
 allFstData = test1_fst+test2_fst+test3_fst+blocked_fst+no_blocked_fst+no_prior_fst
 
-# makeplot(allDurData, test1_dur, test2_dur, test3_dur, blocked_dur, no_blocked_dur, no_prior_dur, "Mean Duration", "Mean Duration Time / Units")
-# makeplot(allCPUData, test1_cpu,test2_cpu,test3_cpu,blocked_cpu,no_blocked_cpu,no_prior_cpu, "CPU Time", "CPU Time / Units")
-# makeplot(allCtextData, test1_ctext,test2_ctext,test3_ctext,blocked_ctext,no_blocked_ctext,no_prior_ctext, "Context Switches", "No. Context Switches")
-# makeplot(allIdleData, test1_idle,test2_idle,test3_idle,blocked_idle,no_blocked_idle,no_prior_idle, "Idle Time", "Idle Time / Units")
-# makeplot(allFstData, test1_fst,test2_fst,test3_fst,blocked_fst,no_blocked_fst,no_prior_fst, "First Job Finish Time", "Finish Time / Units")
+makeplot(allDurData, "Mean Duration", "Mean Duration Time / Units")
+makeplot(allCPUData, "CPU Time", "CPU Time / Units")
+makeplot(allCtextData, "Context Switches", "No. Context Switches")
+makeplot(allIdleData, "Idle Time", "Idle Time / Units")
+makeplot(allFstData, "First Job Finish Time", "Finish Time / Units")
 
 def makePercentages(lst1, lst2):
 	percent = []
@@ -123,5 +122,5 @@ no_prior_idleper = makePercentages(no_prior_idle, no_prior_cpu)
 
 allIdleperData = test1_idleper+test2_idleper+test3_idleper+blocked_idleper+no_blocked_idleper+no_prior_idleper
 
-makeplot(allFstData,test1_idleper,test2_idleper,test3_idleper,blocked_idleper,no_blocked_idleper,no_prior_idleper, "Percent Idle Time", "% Idle")
+makeplot(allIdleperData, "Percent Idle Time", "% Idle")
 
